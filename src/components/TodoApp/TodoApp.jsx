f07c14f38ab34fa1e8be6d943ad0dfc5ffc5f656
Naveen Ravi <93857690+Naveenravi07@users.noreply.github.com>
2022-04-05 11:55:25 -0700

    src

---
+++ src/components/TodoApp/TodoApp.jsx
@@ -0,0 +1,75 @@
+import React, {Component} from 'react'
+import "./TodoApp.css"
+import {SiAzurepipelines} from "react-icons/si"
+
+export class TodoApp extends Component {
+
+    state = {
+        value: "",
+        items: []
+    }
+    handleChange = (event) => {
+        this.setState({value: event.target.value})
+    }
+
+    handlesubmit = event => {
+        event.preventDefault()
+        const {value} = this.state
+        const allitemstemp = this.state.items
+
+        allitemstemp.push(value)
+
+        this.setState({items: allitemstemp, value: ""})
+    }
+
+    deleteItem = (id) => {
+        let dummyarray = this.state.items
+        dummyarray.splice(id)
+        this.setState({items: dummyarray})
+    }
+    render() {
+
+        const {value, items} = this.state;
+        console.log(items);
+        return (
+            <div className='todo-container'>
+                <form onSubmit={
+                    this.handlesubmit
+                }>
+                    <div className='todo-inp'>
+                        <h1 className="Heading">TODO APP
+                            <span>&nbsp;&nbsp;</span>
+                            <SiAzurepipelines/>
+                        </h1>
+
+                        <input value={value}
+                            onChange={
+                                this.handleChange
+                            }
+                            type="text"
+                            placeholder='Add Your Todo Here'/>
+
+                    </div>
+                </form>
+
+                <div className='todo-item'>
+                    <ul> {
+                        this.state.items.map((data, index) => (
+                            <li onClick={
+                                    () => this.deleteItem(index)
+                                }
+                                key={index}>
+                                {data}
+                                <i className='fa-solid fa-trash-can'>
+                               
+                                </i>
+                            </li>
+                        ))
+                    } </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TodoApp
